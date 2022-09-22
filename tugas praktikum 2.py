4484a69de27f89efda288ba2b7e87039583e8d33
pestoLLL <113655363+pestoLLL@users.noreply.github.com>
2022-09-22 17:22:38 +0700

    Add files via upload

---
+++ tugas praktikum 2.py
@@ -0,0 +1,11 @@
+print("Nama    : Jannathan Gerald Mahendra")
+print("Jurusan : Sistem Informasi")
+print("NIM     : 065002200016")
+print()
+panjang = int(input("Masukan Panjang Persegi: "))
+lebar = int(input("Masukan Lebar Persegi  : "))
+
+luas = panjang*lebar
+
+satuan = (input("\nSatuan [Meter/Inci] : "))
+print(f"Luas Persegi Adalah : {luas}" , satuan)
